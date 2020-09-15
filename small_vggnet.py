b20d39ced73f7e154bbfe3c1b50d97556faa6789
hunterking31 <49922062+hunterking31@users.noreply.github.com>
2020-09-15 12:26:00 +0530

    Add files via upload
    Use image downloader applications like Fatkun/Bulk Image Downloader to create a dataset of different categories of your choice. The images will be saved in separate folders copy all folders under a single folder for easy access. I have used 4 different types of dresses you can choose your own class and the number of classes.

---
+++ small_vggnet.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.models import Sequential
+from keras.layers import BatchNormalization,Activation,Conv2D,MaxPooling2D,Dense,Dropout,Flatten
+from keras import backend as K
+
+
+# In[3]:
+
+
+class SmallerVggNet:
+    
+    def build(width,height,depth,classes,finalAct='softmax'):
+        
+        model = Sequential()
+        inputShape = (height,width,depth)
+        chanDim = -1
+        
+        if K.image_data_format() == 'channel_first':
+            inputShape = (depth,height,width)
+            chanDim = 1
+        
+        model.add(Conv2D(32,(3,3),padding='same',input_shape=inputShape))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(3,3)))
+        model.add(Dropout(0.25))
+        
+        model.add(Conv2D(64,(3,3),padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64,(3,3),padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+        
+        model.add(Conv2D(128,(3,3),padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(128,(3,3),padding='same'))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(Dropout(0.25))
+        
+        model.add(Flatten())
+        model.add(Dense(1024))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+        
+        model.add(Dense(classes))
+        model.add(Activation(finalAct))
+        
+        return model
+
+
+# In[ ]:
+
+
+
+
